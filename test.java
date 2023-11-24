54200eebbc70bbea8a651494c7264e07e5376336
ramsan21 <ramsan21@gmail.com>
2023-11-24 18:11:02 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,69 +1,54 @@
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.stereotype.Service;
+import org.apache.hc.client5.http.async.methods.SimpleRequestBuilder;
+import org.apache.hc.client5.http.async.methods.SimpleResponseConsumer;
+import org.apache.hc.client5.http.async.methods.SimpleRequestProducer;
+import org.apache.hc.client5.http.async.methods.AsyncResultCallback;
+import org.apache.hc.client5.http.async.methods.BasicRequestProducer;
+import org.apache.hc.client5.http.async.methods.BasicResponseConsumer;
+import org.apache.hc.core5.concurrent.FutureCallback;
+import org.apache.hc.core5.http.nio.support.BasicRequestConsumer;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpAsyncClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
+import org.apache.hc.client5.http.async.methods.*;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
-@Service
 public class ApiService {
 
     public void makeApiCallsConcurrently() {
         try {
-            // Create a HttpClient with SSL verification disabled
-            CloseableHttpClient httpClient = HttpClients.custom()
-                    .setSSLSocketFactory(new SSLConnectionSocketFactory(SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER))
+            // Create a CloseableHttpAsyncClient with SSL verification disabled
+            CloseableHttpAsyncClient asyncHttpClient = HttpAsyncClients.custom()
+                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
                     .build();
+
+            // Start the async client
+            asyncHttpClient.start();
 
             // Make the first API call
             String firstApiUrl = "https://your-first-api-url"; // Replace with your actual API URL
-            HttpPost firstApiRequest = new HttpPost(firstApiUrl);
-            HttpResponse firstApiResponse = httpClient.execute(firstApiRequest);
+            Future<HttpResponse> firstApiFuture = asyncHttpClient.execute(SimpleRequestBuilder.get(firstApiUrl).build(), new BasicResponseConsumer<>(), null);
+
+            // Wait for the first API call to finish
+            HttpResponse firstApiResponse = firstApiFuture.get();
 
             // TODO: Handle the response of the first API call as needed
-            System.out.println("First API call response code: " + firstApiResponse.getStatusLine().getStatusCode());
-
-            // Close the HttpClient after the first API call
-            httpClient.close();
+            System.out.println("First API call response code: " + firstApiResponse.getCode());
 
             // Now, make 10 concurrent API calls
-            ExecutorService executorService = Executors.newFixedThreadPool(10);
-
             for (int i = 0; i < 10; i++) {
-                executorService.submit(() -> {
-                    try {
-                        CloseableHttpClient concurrentHttpClient = HttpClients.custom()
-                                .setSSLSocketFactory(new SSLConnectionSocketFactory(SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER))
-                                .build();
+                String concurrentApiUrl = "https://your-concurrent-api-url"; // Replace with your actual API URL
+                HttpUriRequestBase request = SimpleRequestBuilder.get(concurrentApiUrl).build();
 
-                        String concurrentApiUrl = "https://your-concurrent-api-url"; // Replace with your actual API URL
-                        HttpPost concurrentApiRequest = new HttpPost(concurrentApiUrl);
-
-                        // Execute the concurrent request
-                        HttpResponse concurrentApiResponse = concurrentHttpClient.execute(concurrentApiRequest);
-
-                        // TODO: Handle the response of the concurrent API call as needed
-                        System.out.println("Concurrent API call response code: " + concurrentApiResponse.getStatusLine().getStatusCode());
-
-                        // Close the HttpClient for the concurrent request
-                        concurrentHttpClient.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
+                asyncHttpClient.execute(request, new BasicResponseConsumer<>(), null);
             }
 
-            // Shutdown the executor service and wait for all tasks to finish
-            executorService.shutdown();
-            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            // Wait for all tasks to finish
+            asyncHttpClient.awaitTermination(30, TimeUnit.SECONDS);
 
-        } catch (Exception e) {
+        } catch (InterruptedException | ExecutionException | IOException e) {
             e.printStackTrace();
         }
     }
