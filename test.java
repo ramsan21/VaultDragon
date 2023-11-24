11aaab684a8bdc292f24a900577fb57bca196fd4
ramsan21 <ramsan21@gmail.com>
2023-11-24 18:28:51 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,55 +1,39 @@
-import org.apache.hc.client5.http.async.methods.SimpleRequestBuilder;
-import org.apache.hc.client5.http.async.methods.SimpleResponseConsumer;
-import org.apache.hc.client5.http.async.methods.SimpleRequestProducer;
-import org.apache.hc.client5.http.async.methods.AsyncResultCallback;
-import org.apache.hc.client5.http.async.methods.BasicRequestProducer;
-import org.apache.hc.client5.http.async.methods.BasicResponseConsumer;
-import org.apache.hc.core5.concurrent.FutureCallback;
-import org.apache.hc.core5.http.nio.support.BasicRequestConsumer;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpAsyncClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
-import org.apache.hc.client5.http.async.methods.*;
+@PostMapping("/reset")
+    public ResponseEntity<Void> reset() {
+        // Trigger POST reset API
+        ResponseEntity<Void> response = restTemplate.postForEntity("https://example.com/reset", null, Void.class);
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+        if (response.getStatusCode().is2xxSuccessful()) {
+            // Make 10 API calls to the same request at once with HTTPS disabled
+            ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 10, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+            List<Future<Void>> futures = new ArrayList<>();
 
-public class ApiService {
-
-    public void makeApiCallsConcurrently() {
-        try {
-            // Create a CloseableHttpAsyncClient with SSL verification disabled
-            CloseableHttpAsyncClient asyncHttpClient = HttpAsyncClients.custom()
-                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-                    .build();
-
-            // Start the async client
-            asyncHttpClient.start();
-
-            // Make the first API call
-            String firstApiUrl = "https://your-first-api-url"; // Replace with your actual API URL
-            Future<HttpResponse> firstApiFuture = asyncHttpClient.execute(SimpleRequestBuilder.get(firstApiUrl).build(), new BasicResponseConsumer<>(), null);
-
-            // Wait for the first API call to finish
-            HttpResponse firstApiResponse = firstApiFuture.get();
-
-            // TODO: Handle the response of the first API call as needed
-            System.out.println("First API call response code: " + firstApiResponse.getCode());
-
-            // Now, make 10 concurrent API calls
             for (int i = 0; i < 10; i++) {
-                String concurrentApiUrl = "https://your-concurrent-api-url"; // Replace with your actual API URL
-                HttpUriRequestBase request = SimpleRequestBuilder.get(concurrentApiUrl).build();
-
-                asyncHttpClient.execute(request, new BasicResponseConsumer<>(), null);
+                Future<Void> future = executor.submit(() -> {
+                    try {
+                        restTemplate.setHttps(false);
+                        restTemplate.postForObject("http://example.com/api", null, Void.class);
+                    } finally {
+                        restTemplate.setHttps(true);
+                    }
+                });
+                futures.add(future);
             }
 
-            // Wait for all tasks to finish
-            asyncHttpClient.awaitTermination(30, TimeUnit.SECONDS);
-
-        } catch (InterruptedException | ExecutionException | IOException e) {
-            e.printStackTrace();
+            // Wait for all 10 API calls to complete
+            for (Future<Void> future : futures) {
+                try {
+                    future.get();
+                } catch (Exception e) {
+                    // Handle any exceptions that occur during the API calls
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            // Handle the error response from the POST reset API
+            System.err.println("Error triggering POST reset API: " + response.getStatusCode());
         }
+
+        return ResponseEntity.ok().build();
     }
 }
