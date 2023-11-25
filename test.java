f46283929a6820dd79cce817a77698b526d94131
ramsan21 <ramsan21@gmail.com>
2023-11-25 12:10:47 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,39 +1,32 @@
-@PostMapping("/reset")
-    public ResponseEntity<Void> reset() {
-        // Trigger POST reset API
-        ResponseEntity<Void> response = restTemplate.postForEntity("https://example.com/reset", null, Void.class);
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
 
-        if (response.getStatusCode().is2xxSuccessful()) {
-            // Make 10 API calls to the same request at once with HTTPS disabled
-            ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 10, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
-            List<Future<Void>> futures = new ArrayList<>();
-
-            for (int i = 0; i < 10; i++) {
-                Future<Void> future = executor.submit(() -> {
-                    try {
-                        restTemplate.setHttps(false);
-                        restTemplate.postForObject("http://example.com/api", null, Void.class);
-                    } finally {
-                        restTemplate.setHttps(true);
-                    }
-                });
-                futures.add(future);
-            }
-
-            // Wait for all 10 API calls to complete
-            for (Future<Void> future : futures) {
-                try {
-                    future.get();
-                } catch (Exception e) {
-                    // Handle any exceptions that occur during the API calls
-                    e.printStackTrace();
+public class SSLUtils {
+    public static void disableSSLValidation() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
                 }
-            }
-        } else {
-            // Handle the error response from the POST reset API
-            System.err.println("Error triggering POST reset API: " + response.getStatusCode());
-        }
 
-        return ResponseEntity.ok().build();
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Disable hostname verification
+            HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+SSLUtils.disableSSLValidation();
