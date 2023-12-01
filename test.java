dd124133c8ee64deeba4906a9a8a7d0edcc8180b
ramsan21 <ramsan21@gmail.com>
2023-12-01 22:01:14 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,16 +1,19 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
+@EnableAsync
 public class MySpringBootApplication {
 
     public static void main(String[] args) {
@@ -18,8 +21,8 @@ public class MySpringBootApplication {
     }
 
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
 
@@ -37,7 +40,7 @@ class MyController {
 
     @PostMapping("/send-post-request")
     public void sendPostRequest(@RequestBody String requestBody) {
-        // Asynchronously send POST requests to both URLs using MyService
+        // Asynchronously send POST requests to both URLs using MyService and ExecutorService
         CompletableFuture<String> response1 = myService.sendAsyncPostRequest(apiUrl1, requestBody);
         CompletableFuture<String> response2 = myService.sendAsyncPostRequest(apiUrl2, requestBody);
 
@@ -53,18 +56,19 @@ class MyController {
 @Service
 class MyService {
 
-    private final WebClient webClient;
+    private final RestTemplate restTemplate;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(2); // Adjust the pool size as needed
 
-    public MyService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.build();
+    public MyService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public CompletableFuture<String> sendAsyncPostRequest(String apiUrl, String requestBody) {
-        return webClient.post()
-                .uri(apiUrl)
-                .body(BodyInserters.fromValue(requestBody))
-                .retrieve()
-                .bodyToMono(String.class)
-                .toFuture();
+        return CompletableFuture.supplyAsync(() -> {
+            // Perform the POST request using RestTemplate
+            String response = restTemplate.postForObject(apiUrl, requestBody, String.class);
+            System.out.println("Received response from " + apiUrl);
+            return response;
+        }, executorService);
     }
 }
