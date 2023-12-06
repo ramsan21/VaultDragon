388a94aeb3bef1a302c7f196f6e68556e9430881
ramsan21 <ramsan21@gmail.com>
2023-12-06 15:40:12 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,112 +1,123 @@
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.crypto.Cipher;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.interfaces.RSAPublicKey;
+import java.security.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-class AsymCipherHandlerTest {
+@RunWith(MockitoJUnitRunner.class)
+public class AsymCipherHandlerTest {
 
     @Mock
-    private RSAPublicKey mockPublicKey;
-
+    private Cipher mockCipher;
+    @Mock
+    private PublicKey mockPublicKey;
     @Mock
     private PrivateKey mockPrivateKey;
 
-    @InjectMocks
-    private AsymCipherHandler cipherHandler;
+    private AsymCipherHandler handler;
 
-    @Test
-    void testDecrypt() throws GeneralSecurityException, IOException {
-        // Set up the test
-        byte[] cipherText = "encryptedText".getBytes();
-        cipherHandler.setPrivateKey(mockPrivateKey);
-
-        Cipher mockCipher = mock(Cipher.class);
-        when(mockCipher.getBlockSize()).thenReturn(16); // Set your desired block size
-        when(cipherHandler.getCipherInstance()).thenReturn(mockCipher);
-
-        // Perform the test
-        byte[] decryptedText = cipherHandler.decrypt(cipherText);
-
-        // Verify that the decryption works as expected
-        // Add assertions based on your specific scenario
-        assertNotNull(decryptedText);
+    @Before
+    public void setUp() {
+        handler = new AsymCipherHandler("RSA");
+        when(mockCipher.getBlockSize()).thenReturn(-1);
     }
 
     @Test
-    void testEncrypt() throws GeneralSecurityException {
-        // Set up the test
-        byte[] plainText = "originalText".getBytes();
-        cipherHandler.setPublicKey(mockPublicKey);
-
-        Cipher mockCipher = mock(Cipher.class);
-        when(mockCipher.getBlockSize()).thenReturn(16); // Set your desired block size
-        when(cipherHandler.getCipherInstance()).thenReturn(mockCipher);
-
-        // Perform the test
-        byte[] encryptedText = cipherHandler.encrypt(plainText);
-
-        // Verify that the encryption works as expected
-        // Add assertions based on your specific scenario
-        assertNotNull(encryptedText);
-    }
-
-    // Add more tests for edge cases, setter/getter methods, etc.
-
-    @Test
-    void testSetPrivateKey() {
-        // Set up the test
-        PrivateKey privateKey = mock(PrivateKey.class);
-
-        // Perform the test
-        cipherHandler.setPrivateKey(privateKey);
-
-        // Verify that the private key is set correctly
-        assertEquals(privateKey, cipherHandler.getPrivatekey());
+    public void testConstructorWithAlgorithm() {
+        assertEquals("RSA", handler.algorithm);
+        assertNull(handler.provider);
+        assertNull(handler.publickey);
+        assertNull(handler.privatekey);
     }
 
     @Test
-    void testSetPublicKey() {
-        // Set up the test
-        PublicKey publicKey = mock(PublicKey.class);
-
-        // Perform the test
-        cipherHandler.setPublicKey(publicKey);
-
-        // Verify that the public key is set correctly
-        assertEquals(publicKey, cipherHandler.getPublickey());
+    public void testConstructorWithAlgorithmAndProvider() {
+        handler = new AsymCipherHandler("RSA", "BC");
+        assertEquals("RSA", handler.algorithm);
+        assertEquals("BC", handler.provider);
+        assertNull(handler.publickey);
+        assertNull(handler.privatekey);
     }
-}
 
-@Test
-    void testDecryptWithReflection() throws Exception {
-        // Set up the test
-        byte[] cipherText = "encryptedText".getBytes();
-        PrivateKey mockPrivateKey = mock(PrivateKey.class);
-
-        AsymCipherHandler cipherHandler = new AsymCipherHandler("algorithm");
-        cipherHandler.setPrivateKey(mockPrivateKey);
-
-        // Use reflection to access the private method
-        Method method = AsymCipherHandler.class.getDeclaredMethod("getCipherInstance");
-        method.setAccessible(true);
-
-        Cipher mockCipher = mock(Cipher.class);
-        when(mockCipher.getBlockSize()).thenReturn(16); // Set your desired block size
-        when(method.invoke(cipherHandler)).thenReturn(mockCipher);
-
-        // Perform the test
-        byte[] decryptedText = cipherHandler.decrypt(cipherText);
-
-        // Verify that the decryption works as expected
-        // Add assertions based on your specific scenario
-        assertEquals("decryptedText", new String(decryptedText));
+    @Test(expected = RuntimeException.class)
+    public void testDecryptWithNullPrivateKey() throws Exception {
+        handler.decrypt(new byte[1]);
     }
+
+    @Test
+    public void testDecryptWithRSAECBAlgorithm() throws Exception {
+        when(mockCipher.getAlgorithm()).thenReturn("RSA/ECB/PKCS1Padding");
+        when(((RSAPublicKey) mockPublicKey).getModulus()).thenReturn(2048);
+        handler.setPrivateKey(mockPrivateKey);
+        handler.setPublicKey(mockPublicKey);
+        handler.decrypt(new byte[1]);
+
+        verify(mockCipher).init(Cipher.DECRYPT_MODE, mockPrivateKey);
+        verify(mockCipher).getBlockSize();
+        verify(mockCipher).doFinal(any(byte[].class));
+    }
+
+    @Test
+    public void testDecryptWithRSAAlgorithm() throws Exception {
+        when(mockCipher.getAlgorithm()).thenReturn("RSA");
+        when(((RSAPublicKey) mockPublicKey).getModulus()).thenReturn(2048);
+        handler.setPrivateKey(mockPrivateKey);
+        handler.setPublicKey(mockPublicKey);
+        handler.decrypt(new byte[1]);
+
+        verify(mockCipher).init(Cipher.DECRYPT_MODE, mockPrivateKey);
+        verify(mockCipher).getBlockSize();
+        verify(mockCipher).doFinal(any(byte[].class));
+    }
+
+    @Test
+    public void testDecryptWithUnknownAlgorithm() throws Exception {
+        when(mockCipher.getAlgorithm()).thenReturn("UNKNOWN");
+        handler.setPrivateKey(mockPrivateKey);
+        handler.setPublicKey(mockPublicKey);
+
+        try {
+            handler.decrypt(new byte[1]);
+            fail("Expected exception");
+        } catch (RuntimeException e) {
+            assertEquals("Unknow blocksize Error!!!", e.getMessage());
+        }
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testEncryptWithNullPublicKey() throws Exception {
+        handler.encrypt(new byte[1]);
+    }
+
+    @Test
+    public void testEncryptWithRSAAlgorithm() throws Exception {
+        when(mockCipher.getAlgorithm()).thenReturn("RSA");
+        when(((RSAPublicKey) mockPublicKey).getModulus()).thenReturn(1024);
+        handler.setPublicKey(mockPublicKey);
+        handler.encrypt(new byte[1]);
+
+        verify(mockCipher).init(Cipher.ENCRYPT_MODE, mockPublicKey);
+        verify(mockCipher).getBlockSize();
+        verify(mockCipher).doFinal(any(byte[].class));
+    }
+
+    @Test
+    public void testSetPrivateKey() {
+        handler.setPrivateKey(mockPrivateKey);
+        assertEquals(mockPrivateKey, handler.getPrivatekey());
+    }
+
+    @Test
+    public void testSetPublicKey() {
+        handler.setPublicKey(mockPublicKey);
+        assertEquals(mockPublicKey, handler.getPublickey());
+    }
+
+    @Test
