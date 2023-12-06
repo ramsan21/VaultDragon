8591bb13f7a9bbcf78dbc351ac421d25f5ac9fc1
ramsan21 <ramsan21@gmail.com>
2023-12-06 11:39:57 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,80 +1,75 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.test.util.ReflectionTestUtils;
 
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.SignatureException;
-import java.security.cert.Certificate;
-import java.util.Collections;
+import static org.mockito.Mockito.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-
-class JWTTest {
+class UAASRsaSHA256VerifierTest {
 
     @Mock
-    private KeyHelper keyHelper;
+    private SignatureHandler mockHandler;
 
     @InjectMocks
-    private JWT jwt;
+    private UAASRsaSHA256Verifier verifier;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    void testVerifySignature_Success() throws CryptoException {
+        // Mocking behavior
+        byte[] claims = "claims".getBytes();
+        byte[] signature = "signature".getBytes();
+        when(mockHandler.verify(claims, signature)).thenReturn(true);
+
+        // Perform the test
+        verifier.verifySignature(claims, signature);
+
+        // Verify that the logger.debug and other assertions as needed
+        // ...
+
+        // Example assertions using Mockito
+        verify(mockHandler, times(1)).verify(claims, signature);
+    }
+
+    @Test
+    void testVerifySignature_Failure() {
+        // Mocking behavior
+        byte[] claims = "claims".getBytes();
+        byte[] signature = "signature".getBytes();
+        when(mockHandler.verify(claims, signature)).thenReturn(false);
+
+        // Perform the test and expect CryptoException
+        assertThrows(CryptoException.class, () -> verifier.verifySignature(claims, signature));
+
+        // Verify that the logger.debug and other assertions as needed
+        // ...
+
+        // Example assertions using Mockito
+        verify(mockHandler, times(1)).verify(claims, signature);
+    }
+
+    @Test
+    void testVerifySignature_Exception() {
+        // Mocking behavior to throw an exception
+        byte[] claims = "claims".getBytes();
+        byte[] signature = "signature".getBytes();
+        when(mockHandler.verify(claims, signature)).thenThrow(new RuntimeException("Simulated exception"));
+
+        // Perform the test and expect CryptoException
+        assertThrows(CryptoException.class, () -> verifier.verifySignature(claims, signature));
+
+        // Verify that the logger.debug and other assertions as needed
+        // ...
+
+        // Example assertions using Mockito
+        verify(mockHandler, times(1)).verify(claims, signature);
+    }
+
+    @Test
+    void testConstructor() {
+        // Initialize the mocks
         MockitoAnnotations.openMocks(this);
-        // Initialize private fields using ReflectionTestUtils
-        ReflectionTestUtils.setField(jwt, "ISSUER", "UAAS");
-        ReflectionTestUtils.setField(jwt, "AUDIENCE", "S2B");
-        ReflectionTestUtils.setField(jwt, "PAYLOAD", "payload");
-        ReflectionTestUtils.setField(jwt, "JWT_ISSUANCE_LATENCY_LIMIT", 1000 * 30L);
-        ReflectionTestUtils.setField(jwt, "JWT_TOKEN_TIMEOUT_DURATION_30MINUTES", 1000L * 60L * 30L);
-        ReflectionTestUtils.setField(jwt, "gson", new Gson());
-    }
 
-    @Test
-    void testCreateJwtToken() throws Exception {
-        // Mock dependencies
-        when(keyHelper.getKeyStore(anyString(), anyString(), anyString(), anyString())).thenReturn(Mockito.mock(KeyStore.class));
-        when(keyHelper.getKey(anyString(), anyString())).thenReturn(Mockito.mock(PrivateKey.class));
-        when(keyHelper.getCertificate(anyString())).thenReturn(Mockito.mock(Certificate.class));
-
-        // Mock current time
-        long currentTime = System.currentTimeMillis();
-        ReflectionTestUtils.setField(jwt, "currentTimeMillis", currentTime);
-
-        // Call the method
-        String token = jwt.createJwtToken("payload", 1000L);
-
-        // Verify the result or perform assertions as needed
+        // Perform any additional assertions related to the constructor
         // ...
-
-        // Example assertion
-        assertEquals("expectedTokenValue", token);
-    }
-
-    @Test
-    void testVerifyTokenAndExtractClaims() throws Exception {
-        // Mock dependencies
-        when(keyHelper.getKeyStore(anyString(), anyString(), anyString(), anyString())).thenReturn(Mockito.mock(KeyStore.class));
-        when(keyHelper.getKey(anyString(), anyString())).thenReturn(Mockito.mock(PrivateKey.class));
-        when(keyHelper.getCertificate(anyString())).thenReturn(Mockito.mock(Certificate.class));
-
-        // Mock current time
-        long currentTime = System.currentTimeMillis();
-        ReflectionTestUtils.setField(jwt, "currentTimeMillis", currentTime);
-
-        // Call the method
-        Object result = jwt.verifyTokenAndExtractClaims("tokenString", Object.class);
-
-        // Verify the result or perform assertions as needed
-        // ...
-
-        // Example assertion
-        assertEquals("expectedResult", result);
     }
 }
