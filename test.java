fbe10071753e1554f880c847d620e6e265943288
ramsan21 <ramsan21@gmail.com>
2023-12-08 13:33:06 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,22 +1,23 @@
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
-
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import java.security.GeneralSecurityException;
-import java.util.Optional;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
-public class SymmCipherHandlerTest {
+@ExtendWith(MockitoExtension.class)
+@PrepareForTest({SymmCipherHandler.class})
+class SymmCipherHandlerTest {
 
     @Mock
     private Cipher mockCipher;
@@ -27,21 +28,23 @@ public class SymmCipherHandlerTest {
     @InjectMocks
     private SymmCipherHandler symmCipherHandler;
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
+    @BeforeEach
+    void setUp() {
+        PowerMockito.mockStatic(Cipher.class);
+        when(Cipher.getInstance(anyString(), anyString())).thenReturn(mockCipher);
+        when(Cipher.getInstance(anyString())).thenReturn(mockCipher);
     }
 
     @Test
-    public void testDecrypt() throws Exception {
+    void testDecrypt() throws Exception {
         byte[] cipherText = "TestCipherText".getBytes();
         byte[] expectedPlainText = "TestPlainText".getBytes();
 
         when(mockCipher.getAlgorithm()).thenReturn("AES/CBC/PKCS5Padding");
         when(mockCipher.doFinal(cipherText)).thenReturn(expectedPlainText);
 
-        Whitebox.setInternalState(symmCipherHandler, "algorithm", "AES");
-        Whitebox.setInternalState(symmCipherHandler, "secretkey", mockSecretKey);
+        symmCipherHandler.setAlgorithm("AES");
+        symmCipherHandler.setSecretKey(mockSecretKey);
 
         byte[] result = symmCipherHandler.decrypt(cipherText);
 
@@ -50,15 +53,15 @@ public class SymmCipherHandlerTest {
     }
 
     @Test
-    public void testEncrypt() throws Exception {
+    void testEncrypt() throws Exception {
         byte[] plainText = "TestPlainText".getBytes();
         byte[] expectedCipherText = "TestCipherText".getBytes();
 
         when(mockCipher.getAlgorithm()).thenReturn("AES/CBC/PKCS5Padding");
         when(mockCipher.doFinal(plainText)).thenReturn(expectedCipherText);
 
-        Whitebox.setInternalState(symmCipherHandler, "algorithm", "AES");
-        Whitebox.setInternalState(symmCipherHandler, "secretkey", mockSecretKey);
+        symmCipherHandler.setAlgorithm("AES");
+        symmCipherHandler.setSecretKey(mockSecretKey);
 
         byte[] result = symmCipherHandler.encrypt(plainText);
 
@@ -67,24 +70,26 @@ public class SymmCipherHandlerTest {
     }
 
     @Test
-    public void testSetIv() {
+    void testSetIv() {
         byte[] iv = "TestIV".getBytes();
 
         symmCipherHandler.setIv(iv);
 
-        byte[] actualIv = Whitebox.getInternalState(symmCipherHandler, "iv");
+        byte[] actualIv = symmCipherHandler.getIv();
         assertArrayEquals(iv, actualIv);
     }
 
     @Test
-    public void testGetDefaultIV() {
+    void testGetDefaultIV() {
         int totalIVByte = 16;
         byte[] expectedIV = new byte[totalIVByte];
         for (int i = 0; i < totalIVByte; i++) {
             expectedIV[i] = (byte) 0xf3;
         }
 
-        byte[] result = Whitebox.invokeMethod(symmCipherHandler, "getDefaultIV", totalIVByte);
+        byte[] result = PowerMockito.method(SymmCipherHandler.class, "getDefaultIV", int.class)
+                .withArguments(totalIVByte)
+                .invoke();
 
         assertArrayEquals(expectedIV, result);
     }
