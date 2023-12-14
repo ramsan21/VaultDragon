9f276c48e0b831c6aa3f81d42f5d347307b7edfd
ramsan21 <ramsan21@gmail.com>
2023-12-14 10:46:08 +0800

    Update test.java

--- test.java
+++ test.java
@@ -4,22 +4,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.api.mockito.PowerMockito;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@PrepareForTest({ImportPublicKeyFileCommand.class, BufferedReader.class})
-public class ImportPublicKeyFileCommandTest {
+class ImportPublicKeyFileCommandTest {
 
     @Mock
     private wtValidationKeyRepository jwtValidationKeyRepository;
@@ -27,28 +25,16 @@ public class ImportPublicKeyFileCommandTest {
     @InjectMocks
     private ImportPublicKeyFileCommand importPublicKeyFileCommand;
 
-    @Mock
-    private BufferedReader bufferedReader;
-
     @BeforeEach
-    public void setUp() throws IOException {
-        // Mock static method for BufferedReader instantiation
-        PowerMockito.mockStatic(BufferedReader.class);
-        when(BufferedReader.class.getResourceAsStream(any(String.class))).thenReturn(null);
-        when(BufferedReader.class.getResourceAsStream(any(String.class))).thenReturn(null);
-
-        // Mock constructor of FileReader
-        PowerMockito.whenNew(FileReader.class).withAnyArguments().thenReturn(new FileReader("dummyFile"));
-
-        // Set up a mock BufferedReader for FileReader
-        when(bufferedReader.readLine()).thenReturn("-----BEGIN PUBLIC KEY-----", "public_key_content", "-----END PUBLIC KEY-----", null);
+    void setUp() throws IOException {
+        // Mock BufferedReader behavior
+        BufferedReader bufferedReader = new BufferedReader(new StringReader("-----BEGIN PUBLIC KEY-----\npublic_key_content\n-----END PUBLIC KEY-----"));
+        when(jwtValidationKeyRepository.findByIssuer(anyString())).thenReturn(Optional.empty());
+        when(importPublicKeyFileCommand.createBufferedReader(anyString())).thenReturn(bufferedReader);
     }
 
     @Test
-    public void testImportPublicKey_Success() {
-        // Mock repository to return an empty Optional, indicating the issuer doesn't exist
-        when(jwtValidationKeyRepository.findByIssuer(anyString())).thenReturn(Optional.empty());
-
+    void testImportPublicKey_Success() {
         Map<String, String> args = new HashMap<>();
         args.put("issuer", "testIssuer");
         args.put("fileName", "testFile");
@@ -57,8 +43,7 @@ public class ImportPublicKeyFileCommandTest {
         Map<String, String> result = importPublicKeyFileCommand.with(args);
 
         assertEquals("success", result.get("output"));
-        // Add any additional verifications as needed
     }
 
-    // Add more test cases for different scenarios (e.g., existing issuer without override, invalid parameters, etc.)
+    // Add more test cases for different scenarios
 }
