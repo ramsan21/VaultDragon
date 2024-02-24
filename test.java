18f5554d80d95ebea3ca8429c066f49602d3d65d
ramsan21 <ramsan21@gmail.com>
2024-02-24 21:13:40 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,85 +1,15 @@
-openapi: 3.0.0
-info:
-  title: GAPI WB Rest Service
-  description: API documentation for the GAPI WB Rest Service
-  version: 1.0.0
-paths:
-  /v2/gapi-wb-rest/api/decrypt:
-    post:
-      summary: Decrypt a message using GAPI WB Rest
-      description: Endpoint to decrypt a message using GAPI WB Rest.
-      parameters:
-        - name: source_id
-          in: query
-          description: Source ID for decryption
-          required: true
-          schema:
-            type: string
-        - name: message_len
-          in: query
-          description: Length of the message
-          required: true
-          schema:
-            type: integer
-        - name: enc_message
-          in: query
-          description: Encrypted message to be decrypted
-          required: true
-          schema:
-            type: string
-      responses:
-        '200':
-          description: Successful decryption
-          content:
-            application/json:
-              example:
-                status: "Success"
-                dec_message: "Decrypted message"
-                message_len: 20
-        '400':
-          description: Bad Request
-          content:
-            application/json:
-              example:
-                status: "Error"
-                error_message: "Invalid request format"
+CREATE SEQUENCE PublicKeySeq
+    START WITH 1
+    INCREMENT BY 1
+    NOMAXVALUE
+    NOCYCLE;
 
-  /v2/gapi-wb-rest/api/encrypt:
-    post:
-      summary: Encrypt a message using GAPI WB Rest
-      description: Endpoint to encrypt a message using GAPI WB Rest.
-      parameters:
-        - name: source_id
-          in: query
-          description: Source ID for encryption
-          required: true
-          schema:
-            type: string
-        - name: message_len
-          in: query
-          description: Length of the message
-          required: true
-          schema:
-            type: integer
-        - name: plain_message
-          in: query
-          description: Plain message to be encrypted
-          required: true
-          schema:
-            type: string
-      responses:
-        '200':
-          description: Successful encryption
-          content:
-            application/json:
-              example:
-                status: "Success"
-                enc_message: "Encrypted message"
-                message_len: 30
-        '400':
-          description: Bad Request
-          content:
-            application/json:
-              example:
-                status: "Error"
-                error_message: "Invalid request format"
+CREATE TABLE PublicKeyTable (
+    ID NUMBER DEFAULT PublicKeySeq.NEXTVAL NOT NULL,
+    Identity VARCHAR2(50) NOT NULL,
+    KeyId VARCHAR2(50) NOT NULL,
+    Filename VARCHAR2(255) NOT NULL,
+    KeyBlob BLOB,
+    IsBankId NUMBER(1) DEFAULT 0, -- 0 for clientid, 1 for bankid
+    CONSTRAINT PublicKeyTable_PK PRIMARY KEY (ID)
+);
