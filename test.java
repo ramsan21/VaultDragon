03fbd086b3836125d02851699851a67eadbd6551
ramsan21 <ramsan21@gmail.com>
2024-02-29 09:53:55 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,54 +1,26 @@
-import org.bouncycastle.bcpg.SignatureSubpacketTags;
-import org.bouncycastle.openpgp.*;
-
-import java.io.*;
-import java.util.Date;
-
-public class PGPKeyExpiration {
-
-    public static void main(String[] args) {
-        String keyRingFilePath = "path/to/your/keyring.gpg";
-
-        try {
-            Date expirationDate = getKeyExpirationDate(keyRingFilePath);
-            if (expirationDate != null) {
-                System.out.println("Key Expiration Date: " + expirationDate);
-            } else {
-                System.out.println("Key does not have an expiration date.");
+public PGPPublicKey findEncryptionKey(List<CustomerKey> customerKeys) {
+        for (CustomerKey customerKey : customerKeys) {
+            PGPPublicKey publicKey = extractEncryptionKey(customerKey.getPublicKeyData());
+            if (publicKey != null) {
+                return publicKey;
             }
-        } catch (IOException | PGPException e) {
-            e.printStackTrace();
         }
+        return null; // No encryption key found in any key
     }
 
-    private static Date getKeyExpirationDate(String keyRingFilePath) throws IOException, PGPException {
-        try (InputStream keyRingStream = new FileInputStream(keyRingFilePath)) {
-            PGPPublicKeyRingCollection keyRingCollection = new PGPPublicKeyRingCollection(keyRingStream, new JcaKeyFingerprintCalculator());
-
+    private PGPPublicKey extractEncryptionKey(byte[] publicKeyData) {
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(publicKeyData)) {
+            PGPPublicKeyRingCollection keyRingCollection = new PGPPublicKeyRingCollection(inputStream);
+            
             for (PGPPublicKeyRing keyRing : keyRingCollection) {
                 for (PGPPublicKey publicKey : keyRing) {
-                    Date expirationDate = getSignatureExpirationDate(publicKey.getSelfSignature());
-                    if (expirationDate != null) {
-                        return expirationDate;
+                    if (publicKey.isEncryptionKey()) {
+                        return publicKey;
                     }
                 }
             }
+        } catch (IOException | PGPException e) {
+            e.printStackTrace(); // Handle exception appropriately
         }
-
-        return null;
+        return null; // No encryption key found in this key
     }
-
-    private static Date getSignatureExpirationDate(PGPSignature signature) {
-        PGPSignatureSubpacketVector subpackets = signature.getHashedSubPackets();
-        if (subpackets != null) {
-            SignatureSubpacket subpacket = subpackets.getSubpacket(SignatureSubpacketTags.KEY_EXPIRE_TIME);
-            if (subpacket != null) {
-                long expirationTime = subpacket.getTime();
-                if (expirationTime > 0) {
-                    return new Date(expirationTime * 1000L);
-                }
-            }
-        }
-        return null;
-    }
-}
