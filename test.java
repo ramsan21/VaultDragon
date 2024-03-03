c5623a998bbf5e2b8c72c0501da4580302ad48af
ramsan21 <ramsan21@gmail.com>
2024-03-03 19:43:56 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,83 +1,67 @@
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+public MessageResponse signFile(SignFileRequest req) throws Exception {
+    MessageResponse response = new MessageResponse();
+    String inputFile = FilenameUtils.getFullPath(req.getInputFile()) + FilenameUtils.getName(req.getInputFile());
+    String outputFile = FilenameUtils.getFullPath(req.getOutputFile()) + FilenameUtils.getName(req.getOutputFile());
 
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.List;
+    try (FileOutputStream out = new FileOutputStream(outputFile);
+         ArmoredOutputStream outArmor = req.isArmor() ? new ArmoredOutputStream(out) : null;
+         PGPCompressedDataGenerator pgpCompressedDataGenerator = new PGPCompressedDataGenerator(CompressionAlgorithmTags.ZLIB);
+         BCPGOutputStream bout = new BCPGOutputStream(req.isArmor() ? outArmor : out);
+         PGPLiteralDataGenerator lGen = new PGPLiteralDataGenerator();
+         OutputStream lOut = lGen.open(bout, PGPLiteralData.BINARY, new File(inputFile));
+         FileInputStream fIn = new FileInputStream(new File(inputFile))) {
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+        PGPSignatureGenerator pgpSignatureGenerator = setupPGPSignatureGenerator(req);
 
-public class BankKeyServiceTest {
+        pgpSignatureGenerator.generateOnePassVersion(false).encode(bout);
 
-    @Mock
-    private BankKeyRepository repository;
+        int ch;
+        while ((ch = fIn.read()) >= 0) {
+            lOut.write(ch);
+            pgpSignatureGenerator.update((byte) ch);
+        }
 
-    @InjectMocks
-    private BankKeyService bankKeyService;
+        pgpSignatureGenerator.generate().encode(bout);
 
-    @Test
-    public void testSaveCurrentBankKey() {
-        BankKey.BankKeyBuilder bankKeyBuilderMock = Mockito.mock(BankKey.BankKeyBuilder.class);
-        when(bankKeyService.bankKeyBuilder()).thenReturn(bankKeyBuilderMock);
+        response.setStatusCode(StatusCode.SUCCESS.getCode());
+        response.setSuccessMessage("Signed file [" + outputFile + "] created successfully.");
+        response.setOutFileName(outputFile);
+        log.info("Sign successful");
 
-        bankKeyService.saveCurrentBankkey();
-
-        verify(repository, times(1)).save(any(BankKey.class));
-        verify(bankKeyService, times(1)).clearCurrentBankKey();
+    } catch (Exception e) {
+        log.error(e.getClass().getName(), e);
+        response.setStatusCode(StatusCode.STAR_FUNC_FAIL.getCode());
+        response.setErrorMessage("Error Msg:" + e.getMessage() + " PGP Signing Failed");
     }
 
-    @Test
-    public void testClearCurrentBankKey() {
-        bankKeyService.clearCurrentBankKey();
-
-        // Add assertions if needed
-    }
-
-    @Test
-    public void testBankKeyBuilderWithNonNullThreadLocal() {
-        BankKey.BankKeyBuilder expectedBuilder = BankKey.builder().createdon(Timestamp.from(Instant.now()));
-        bankKeyService.bankKeyThreadLocal.set(expectedBuilder);
-
-        BankKey.BankKeyBuilder actualBuilder = bankKeyService.bankKeyBuilder();
-
-        assertEquals(expectedBuilder, actualBuilder);
-    }
-
-    @Test
-    public void testBankKeyBuilderWithNullThreadLocal() {
-        bankKeyService.bankKeyThreadLocal.set(null);
-
-        BankKey.BankKeyBuilder actualBuilder = bankKeyService.bankKeyBuilder();
-
-        // Add assertions if needed
-    }
-
-    @Test
-    public void testGetBankkey() {
-        String user = "testUser";
-        List<BankKey> expectedBankKeys = Arrays.asList(new BankKey(), new BankKey());
-
-        when(repository.findyUser(user)).thenReturn(expectedBankKeys);
-
-        List<BankKey> actualBankKeys = bankKeyService.getBankkey(user);
-
-        assertEquals(expectedBankKeys, actualBankKeys);
-    }
-
-    @Test
-    public void testGetBankKeyByKeyId() {
-        String keyId = "testKeyId";
-        List<BankKey> expectedBankKeys = Arrays.asList(new BankKey(), new BankKey());
-
-        when(repository.findBykeyId(keyId)).thenReturn(expectedBankKeys);
-
-        List<BankKey> actualBankKeys = bankKeyService.getBankKeyByKeyId(keyId);
-
-        assertEquals(expectedBankKeys, actualBankKeys);
-    }
+    return response;
 }
+
+private PGPSignatureGenerator setupPGPSignatureGenerator(SignFileRequest req) throws PGPException, NoSuchAlgorithmException, NoSuchProviderException {
+    PGPPrivateKey pgpPrivateKey = this.keyChainHandler.findSecretKey(req.getIdentity());
+    if (pgpPrivateKey == null) {
+        throw new PGPException("Unable to get the secret key from keyring. Identity=" + req.getIdentity());
+    }
+
+    PGPPublicKey pgpPublicKey = this.keyChainHandler.getBankMastKey(req.getIdentity());
+    Iterator<String> it = pgpPublicKey.getUserIDs();
+    log.info("Hash algorithm used for signing = " + req.getHashAlgo());
+
+    PGPSignatureGenerator pgpSignatureGenerator = new PGPSignatureGenerator(
+            new JcaPGPContentSignerBuilder(PublicKeyAlgorithmTags.RSA_GENERAL,
+                    algoUtil.getSymmetricCipherValueByName(req.getHashAlgo()))
+                    .setProvider(config.getProviders())
+                    .setDigestProvider(config.getProviders()));
+
+    pgpSignatureGenerator.init(PGPSignature.BINARY_DOCUMENT, pgpPrivateKey);
+
+    if (it.hasNext()) {
+        String user = it.next();
+        PGPSignatureSubpacketGenerator spen = new PGPSignatureSubpacketGenerator();
+        spen.addSignerUserID(false, user.getBytes(StandardCharsets.UTF_8));
+        pgpSignatureGenerator.setHashedSubpackets(spen.generate());
+    }
+
+    return pgpSignatureGenerator;
+}
+
