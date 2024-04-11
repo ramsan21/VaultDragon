2584fd5f3590017cbcd55e9e59ac6764208ba0ee
ramsan21 <ramsan21@gmail.com>
2024-04-12 00:14:50 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,57 +1,24 @@
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
-public class ApiClient {
+// ...
 
-    public void postWithAuthentication(String url, Object requestBody, String username, String password) {
-        RestTemplate restTemplate = new RestTemplate(getCustomClientHttpRequestFactory());
+try {
+    SSLContext sslContext = new SSLContextBuilder()
+                                .loadTrustMaterial(null, (certificate, authType) -> true)
+                                .build();
 
-        // Set up the headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setBasicAuth(username, password);
-
-        // Create the request entity
-        HttpEntity<Object> requestEntity = new HttpEntity<>(requestBody, headers);
-
-        // Make the POST request
-        ResponseEntity<String> responseEntity = restTemplate.exchange(
-                url,
-                HttpMethod.POST,
-                requestEntity,
-                String.class
-        );
-
-        // Handle the response
-        // ...
-    }
-
-    private SimpleClientHttpRequestFactory getCustomClientHttpRequestFactory() {
-        try {
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, new TrustManager[] { new X509TrustManager() {
-                public void checkClientTrusted(X509Certificate[] chain, String authType) {
-                }
-
-                public void checkServerTrusted(X509Certificate[] chain, String authType) {
-                }
-
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-            } }, new java.security.SecureRandom());
-
-            SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
-            clientHttpRequestFactory.setSSLContext(sslContext);
-            return clientHttpRequestFactory;
-        } catch (KeyManagementException | NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+    
+    CloseableHttpClient httpClient = HttpClients.custom()
+                                                .setSSLSocketFactory(socketFactory)
+                                                .build();
+    
+    // Use httpClient as needed for requests
+} catch (Exception e) {
+    e.printStackTrace();
 }
