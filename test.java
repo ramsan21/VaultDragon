ae56b7ecb1b559b24d958de40f9299e020d5e181
ramsan21 <ramsan21@gmail.com>
2024-04-12 01:34:59 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,33 +1,65 @@
-try {
-            RestTemplate restTemplate = new RestTemplate();
-
-            KeyStore keyStore = KeyStore.getInstance("jks");
-
-            File keyFile = new File(KEY_STORE_FILE);
-            FileSystemResource fileSystemResource = new FileSystemResource(keyFile);
-
-            InputStream inputStream = fileSystemResource.getInputStream();
-            keyStore.load(inputStream,
-                    Objects.requireNonNull(KEY_STORE_PASS).toCharArray());
-
-            SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(new SSLContextBuilder()
-                    .loadTrustMaterial(null, new TrustSelfSignedStrategy())
-                    .loadKeyMaterial(keyStore,
-                            KEY_PASS.toCharArray()).build(),
-                    NoopHostnameVerifier.INSTANCE);
-
-            HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory)
-                    .build();
-
-            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-
-            restTemplate.setRequestFactory(requestFactory);
-
-            return restTemplate;
-
-        } catch (Exception e) {
-            logger.debug("SSL keystore exception", e);
-            throw new BlahBlahException(e);
-        }
+<dependencies>
+    <!-- Spring Boot Starter Web -->
+    <dependency>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-web</artifactId>
+    </dependency>
+    <!-- JSch for SSH and SCP -->
+    <dependency>
+        <groupId>com.jcraft</groupId>
+        <artifactId>jsch</artifactId>
+        <version>0.1.55</version>
+    </dependency>
+</dependencies>
 
 
+            package com.example.demo.service;
+
+import com.jcraft.jsch.*;
+import java.nio.file.Path;
+import java.util.Vector;
+
+public class ScpService {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+
+    public ScpService(String host, int port, String username, String password) {
+        this.host = host;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void upload(Path localPath, String remotePath) throws JSchException, SftpException {
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(username, host, port);
+        session.setPassword(password);
+        session.setConfig("StrictHostKeyChecking", "no");
+        session.connect();
+
+        Channel channel = session.openChannel("sftp");
+        channel.connect();
+        ChannelSftp sftpChannel = (ChannelSftp) channel;
+        sftpChannel.put(localPath.toString(), remotePath, ChannelSftp.OVERWRITE);
+        sftpChannel.exit();
+        session.disconnect();
+    }
+
+    public void download(String remotePath, Path localPath) throws JSchException, SftpException {
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(username, host, port);
+        session.setPassword(password);
+        session.setConfig("StrictHostKeyChecking", "no");
+        session.connect();
+
+        Channel channel = session.openChannel("sftp");
+        channel.connect();
+        ChannelSftp sftpChannel = (ChannelSftp) channel;
+        sftpChannel.get(remotePath, localPath.toString());
+        sftpChannel.exit();
+        session.disconnect();
+    }
+}
