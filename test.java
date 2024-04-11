fb10357408c4b878e0f406d050c60853a0a28304
ramsan21 <ramsan21@gmail.com>
2024-04-11 23:22:41 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,28 +1,57 @@
-public void exportPublicKey(String userEmail, String outputFile) {
-        ProcessBuilder processBuilder = new ProcessBuilder(gpgExecutablePath, "--export", "-a", userEmail);
-        processBuilder.redirectOutput(ProcessBuilder.Redirect.PIPE);
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
+public class ApiClient {
+
+    public void postWithAuthentication(String url, Object requestBody, String username, String password) {
+        RestTemplate restTemplate = new RestTemplate(getCustomClientHttpRequestFactory());
+
+        // Set up the headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBasicAuth(username, password);
+
+        // Create the request entity
+        HttpEntity<Object> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        // Make the POST request
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                url,
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+
+        // Handle the response
+        // ...
+    }
+
+    private SimpleClientHttpRequestFactory getCustomClientHttpRequestFactory() {
         try {
-            Process process = processBuilder.start();
-            
-            try (OutputStream fos = new FileOutputStream(outputFile)) {
-                byte[] buffer = new byte[1024];
-                int length;
-                
-                // Read from the output stream of the process
-                while ((length = process.getInputStream().read(buffer)) != -1) {
-                    fos.write(buffer, 0, length);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[] { new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {
                 }
-            }
 
-            int exitCode = process.waitFor();
-            if (exitCode == 0) {
-                System.out.println("Public key successfully exported to " + outputFile);
-            } else {
-                System.err.println("Failed to export public key, GPG exited with code " + exitCode);
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            Thread.currentThread().interrupt(); // Restore the interrupted status
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                }
+
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+            } }, new java.security.SecureRandom());
+
+            SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+            clientHttpRequestFactory.setSSLContext(sslContext);
+            return clientHttpRequestFactory;
+        } catch (KeyManagementException | NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
         }
     }
+}
