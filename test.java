8327f79aa4d8efceb0281cbe66647cdeb5c31620
ramsan21 <ramsan21@gmail.com>
2024-04-12 00:16:26 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,24 +1,20 @@
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
-import javax.net.ssl.SSLContext;
 
 // ...
 
-try {
+RestTemplate restTemplate() throws Exception {
     SSLContext sslContext = new SSLContextBuilder()
-                                .loadTrustMaterial(null, (certificate, authType) -> true)
-                                .build();
+            .loadTrustMaterial(null, (certificate, authType) -> true).build();
 
     SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
-    
-    CloseableHttpClient httpClient = HttpClients.custom()
-                                                .setSSLSocketFactory(socketFactory)
-                                                .build();
-    
-    // Use httpClient as needed for requests
-} catch (Exception e) {
-    e.printStackTrace();
+    CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+    HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
+    return new RestTemplate(factory);
 }
