f20279de0b1d01c3b9e3879d361e7fe987c51341
ramsan21 <ramsan21@gmail.com>
2024-04-12 01:42:10 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,65 +1,56 @@
-<dependencies>
-    <!-- Spring Boot Starter Web -->
-    <dependency>
-        <groupId>org.springframework.boot</groupId>
-        <artifactId>spring-boot-starter-web</artifactId>
-    </dependency>
-    <!-- JSch for SSH and SCP -->
-    <dependency>
-        <groupId>com.jcraft</groupId>
-        <artifactId>jsch</artifactId>
-        <version>0.1.55</version>
-    </dependency>
-</dependencies>
-
-
-            package com.example.demo.service;
+package com.example.demo.service;
 
 import com.jcraft.jsch.*;
 import java.nio.file.Path;
-import java.util.Vector;
 
 public class ScpService {
 
     private String host;
     private int port;
     private String username;
-    private String password;
+    private String privateKeyPath;
 
-    public ScpService(String host, int port, String username, String password) {
+    public ScpService(String host, int port, String username, String privateKeyPath) {
         this.host = host;
         this.port = port;
         this.username = username;
-        this.password = password;
+        this.privateKeyPath = privateKeyPath;
+    }
+
+    private Session createSession() throws JSchException {
+        JSch jsch = new JSch();
+        jsch.addIdentity(privateKeyPath); // Add the private key path here
+
+        Session session = jsch.getSession(username, host, port);
+        session.setConfig("StrictHostKeyChecking", "no"); // Consider handling the host key checking differently for production
+        session.connect();
+
+        return session;
     }
 
     public void upload(Path localPath, String remotePath) throws JSchException, SftpException {
-        JSch jsch = new JSch();
-        Session session = jsch.getSession(username, host, port);
-        session.setPassword(password);
-        session.setConfig("StrictHostKeyChecking", "no");
-        session.connect();
-
+        Session session = createSession();
         Channel channel = session.openChannel("sftp");
         channel.connect();
+
         ChannelSftp sftpChannel = (ChannelSftp) channel;
         sftpChannel.put(localPath.toString(), remotePath, ChannelSftp.OVERWRITE);
+
         sftpChannel.exit();
+        channel.disconnect();
         session.disconnect();
     }
 
     public void download(String remotePath, Path localPath) throws JSchException, SftpException {
-        JSch jsch = new JSch();
-        Session session = jsch.getSession(username, host, port);
-        session.setPassword(password);
-        session.setConfig("StrictHostKeyChecking", "no");
-        session.connect();
-
+        Session session = createSession();
         Channel channel = session.openChannel("sftp");
         channel.connect();
+
         ChannelSftp sftpChannel = (ChannelSftp) channel;
         sftpChannel.get(remotePath, localPath.toString());
+
         sftpChannel.exit();
+        channel.disconnect();
         session.disconnect();
     }
 }
