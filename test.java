91bdbb4e3c53608bc019cbcdf81f7484a523dbdf
ramsan21 <ramsan21@gmail.com>
2024-04-12 17:29:16 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,4 +1,41 @@
-gpg --list-secret-keys --with-colons | grep sec | cut -d: -f5 | xargs -I {} gpg --batch --yes --delete-secret-keys {}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-gpg --list-keys --with-colons | grep pub | cut -d: -f5 | xargs -I {} gpg --batch --yes --delete-keys {}
+public class PGPKeyImporter {
+    public static void importPGPPublicKey(String publicKeyFilePath) {
+        try {
+            // Read the public key from the file
+            byte[] publicKeyBytes = Files.readAllBytes(Paths.get(publicKeyFilePath));
 
+            // Create a ProcessBuilder to run the 'gpg --import' command
+            ProcessBuilder pb = new ProcessBuilder("gpg", "--import");
+            Process process = pb.start();
+
+            // Write the public key bytes to the stdin of the 'gpg --import' process
+            process.getOutputStream().write(publicKeyBytes);
+            process.getOutputStream().flush();
+            process.getOutputStream().close();
+
+            // Read the output from the 'gpg --import' process
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Wait for the process to finish and get the exit code
+            int exitCode = process.waitFor();
+            System.out.println("Process exit code: " + exitCode);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String publicKeyFilePath = "path/to/public/key.asc";
+        importPGPPublicKey(publicKeyFilePath);
+    }
+}
