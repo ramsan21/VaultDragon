46e4c297a47f6926e22ab89653fd337d5844f493
ramsan21 <ramsan21@gmail.com>
2024-04-13 14:38:19 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,30 +1,14 @@
-public static void importPublicKey(String publicKeyFile) {
-        List<String> command = new ArrayList<>();
-        command.add(GPG_PATH);
-        command.add("--batch");
-        command.add("--yes");
-        command.add("--import");
-        command.add(publicKeyFile);
+Session session = jsch.getSession(username, host, port);
+session.setConfig("StrictHostKeyChecking", "no");
 
-        executeCommand(command);
-    }
+// Specify the preferred encryption algorithms
+session.setConfig("kex", "diffie-hellman-group1-sha1,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha1,diffie-hellman-group-exchange-sha256");
+session.setConfig("server_host_key", "ssh-rsa,ssh-dss");
+session.setConfig("cipher.s2c", "aes128-ctr,aes128-cbc,3des-ctr,3des-cbc,blowfish-cbc,aes192-ctr,aes192-cbc,aes256-ctr,aes256-cbc");
+session.setConfig("cipher.c2s", "aes128-ctr,aes128-cbc,3des-ctr,3des-cbc,blowfish-cbc,aes192-ctr,aes192-cbc,aes256-ctr,aes256-cbc");
+session.setConfig("mac.s2c", "hmac-md5,hmac-sha1,hmac-sha2-256,hmac-sha1-96,hmac-md5-96");
+session.setConfig("mac.c2s", "hmac-md5,hmac-sha1,hmac-sha2-256,hmac-sha1-96,hmac-md5-96");
+session.setConfig("compression.s2c", "none");
+session.setConfig("compression.c2s", "none");
 
-    private static void executeCommand(List<String> command) {
-        try {
-            ProcessBuilder processBuilder = new ProcessBuilder(command);
-            Process process = processBuilder.start();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            int exitCode = process.waitFor();
-            System.out.println("Command executed with exit code: " + exitCode);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-Map<String, String> env = processBuilder.environment();
-env.put("GPG_TTY", "/dev/tty");
+session.connect();
