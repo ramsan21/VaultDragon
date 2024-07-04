f591c81bd09ba459d009ef969ec510f7759cda8b
ramsan21 <ramsan21@gmail.com>
2024-07-04 13:26:52 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,16 +1,3 @@
-SELECT 
-    owner,
-    segment_name AS table_name,
-    segment_type,
-    bytes / 1024 / 1024 AS size_mb
-FROM 
-    dba_segments
-WHERE 
-    segment_type = 'TABLE'
-    AND owner = 'HR'
-ORDER BY 
-    size_mb DESC;
-
-
-SELECT DBMS_METADATA.GET_DDL('TABLE', 'EMPLOYEES') AS ddl_script
-FROM dual;
+SELECT owner, table_name, ROUND((num_rows * avg_row_len + 38) / 1024 / 1024, 2) AS size_mb
+FROM all_tables
+ORDER BY size_mb DESC;
