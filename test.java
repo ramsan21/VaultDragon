e26e3a184e9b7a8e31f5bd68760e21b20c74c5dc
ramsan21 <ramsan21@gmail.com>
2024-07-04 14:26:55 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,7 @@
-SELECT DBMS_METADATA.GET_DDL('TABLE', 'YOUR_TABLE_NAME') AS TABLE_DDL,
-       DBMS_METADATA.GET_DDL('INDEX', index_name) AS INDEX_DDL,
-       DBMS_METADATA.GET_DDL('CONSTRAINT', constraint_name) AS CONSTRAINT_DDL
+SELECT DBMS_METADATA.GET_DDL('TABLE', t.table_name) AS TABLE_DDL,
+       DBMS_METADATA.GET_DDL('INDEX', i.index_name) AS INDEX_DDL,
+       DBMS_METADATA.GET_DDL('CONSTRAINT', c.constraint_name) AS CONSTRAINT_DDL
 FROM all_tables t
 LEFT JOIN all_indexes i ON t.table_name = i.table_name
 LEFT JOIN all_constraints c ON t.table_name = c.table_name
 WHERE t.table_name = 'YOUR_TABLE_NAME';
-
-
-SELECT DBMS_METADATA.GET_DDL('TABLE', 'YOUR_TABLE_NAME') AS DDL_SCRIPT
-FROM DUAL;
