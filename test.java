961402dfd95396c9bd6cf2e640f17e648f462009
ramsan21 <ramsan21@gmail.com>
2024-08-21 10:34:36 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,21 +1,17 @@
-#!/bin/bash
+1. Backup files with current date
+/prd/starss/uaas/secrets/hc.cer
+/prd/starss/uaas/secrets/hc-pkcs8.key
 
-# Get current date in YYYYMMDD format
-current_date=$(date +%Y%m%d)
+/prd/starss/uaas/conf_v2/uaas_server.properties
+/prd/starss/uaasadm/conf_v2/uaas_server.properties
 
-# Backup files
-cp /prd/starss/uaas/secrets/hc.cer /prd/starss/uaas/secrets/hc.cer.$current_date.bak
-cp /prd/starss/uaas/secrets/hc-pkcs8.key /prd/starss/uaas/secrets/hc-pkcs8.key.$current_date.bak
-cp /prd/starss/uaas/conf_v2/uaas_server.properties /prd/starss/uaas/conf_v2/uaas_server.properties.$current_date.bak
-cp /prd/starss/uaasadm/conf_v2/uaas_server.properties /prd/starss/uaasadm/conf_v2/uaas_server.properties.$current_date.bak
+copy the files from 
+/tmp/uaas/hc.cer 		to /prd/starss/uaas/secrets/hc.cer
+/tmp/uaas/hc-pkcs8.key 	to /prd/starss/uaas/secrets/hc.cer
 
-# Copy new files
-cp /tmp/uaas/hc.cer /prd/starss/uaas/secrets/hc.cer
-cp /tmp/uaas/hc-pkcs8.key /prd/starss/uaas/secrets/hc-pkcs8.key
-cp /tmp/uaas/uaas_server.properties /prd/starss/uaas/conf_v2/uaas_server.properties
-cp /tmp/uaasadm/uaas_server.properties /prd/starss/uaasadm/conf_v2/uaas_server.properties
+/tmp/uaas/uaas_server.properties to /prd/starss/uaas/conf_v2/uaas_server.properties
+/tmp/uaasadm/uaas_server.properties to /prd/starss/uaasadm/conf_v2/uaas_server.properties
 
-echo "Backup and copy operation completed successfully."
 #!/bin/bash
 
 # Get current date in YYYYMMDD format
