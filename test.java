662a4b04799cae7ee30c3ba194715055c98addc9
ramsan21 <ramsan21@gmail.com>
2024-09-19 12:39:51 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,32 +1,40 @@
-1. Backup files with current date
-/prd/starss/uaas/secrets/hc.cer
-/prd/starss/uaas/secrets/hc-pkcs8.key
-
-/prd/starss/uaas/conf_v2/uaas_server.properties
-/prd/starss/uaasadm/conf_v2/uaas_server.properties
-
-copy the files from 
-/tmp/uaas/hc.cer 		to /prd/starss/uaas/secrets/hc.cer
-/tmp/uaas/hc-pkcs8.key 	to /prd/starss/uaas/secrets/hc.cer
-
-/tmp/uaas/uaas_server.properties to /prd/starss/uaas/conf_v2/uaas_server.properties
-/tmp/uaasadm/uaas_server.properties to /prd/starss/uaasadm/conf_v2/uaas_server.properties
-
 #!/bin/bash
 
-# Get current date in YYYYMMDD format
-current_date=$(date +%Y%m%d)
+# Start maintenance
+echo "Starting maintenance..."
+./prd/starss/maintenance.sh start
 
-# Backup files
-cp /prd/starss/uaas/secrets/hc.cer /prd/starss/uaas/secrets/hc.cer.$current_date.bak
-cp /prd/starss/uaas/secrets/hc-pkcs8.key /prd/starss/uaas/secrets/hc-pkcs8.key.$current_date.bak
-cp /prd/starss/uaas/conf_v2/uaas_server.properties /prd/starss/uaas/conf_v2/uaas_server.properties.$current_date.bak
-cp /prd/starss/uaasadm/conf_v2/uaas_server.properties /prd/starss/uaasadm/conf_v2/uaas_server.properties.$current_date.bak
+# Wait for 20 seconds
+sleep 20
 
-# Copy new files
-cp /tmp/uaas/hc.cer /prd/starss/uaas/secrets/hc.cer
-cp /tmp/uaas/hc-pkcs8.key /prd/starss/uaas/secrets/hc-pkcs8.key
-cp /tmp/uaas/uaas_server.properties /prd/starss/uaas/conf_v2/uaas_server.properties
-cp /tmp/uaasadm/uaas_server.properties /prd/starss/uaasadm/conf_v2/uaas_server.properties
+# Switch user to "nfast" and run installation command
+echo "Switching user to 'nfast' and running install command..."
+sudo -u nfast /opt/nfast/sbin/install
 
-echo "Backup and copy operation completed successfully."
+# Switch to 'jbossadm' user
+echo "Switching user to 'jbossadm'..."
+su - jbossadm <<'EOF'
+
+# Shutdown instances
+echo "Shutting down STARSEC instance..."
+/jboss/tomcat/jws6/instances/SIT_01_CIB_STARSEC_HK/bin/shutdown.sh
+echo "Shutting down STARSECADMIN instance..."
+/jboss/tomcat/jws6/instances/SIT_01_CIB_STARSECADMIN_HK/bin/shutdown.sh
+
+# Wait for 10 seconds
+echo "Waiting for 10 seconds..."
+sleep 10
+
+# Start instances
+echo "Starting STARSECADMIN instance..."
+/jboss/tomcat/jws6/instances/SIT_01_CIB_STARSECADMIN_HK/bin/startup.sh
+echo "Starting STARSEC instance..."
+/jboss/tomcat/jws6/instances/SIT_01_CIB_STARSEC_HK/bin/startup.sh
+
+EOF
+
+# Stop maintenance
+echo "Stopping maintenance..."
+./prd/starss/maintenance.sh stop
+
+echo "Script execution completed."
