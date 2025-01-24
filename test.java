0ffa4a28f6311b443e4ea1dc4aaf11ec56cc2aa0
ramsan21 <ramsan21@gmail.com>
2025-01-24 22:26:55 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,13 @@
-keytool -importcert -file your_certificate.crt -alias your_alias -keystore keystore.jks
+#!/bin/bash
+echo "Stopping Tomcat..."
+<TOMCAT_HOME>/bin/shutdown.sh
+
+echo "Killing remaining Java processes..."
+ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
+
+echo "Clearing temporary files..."
+rm -rf <TOMCAT_HOME>/work/*
+rm -rf <TOMCAT_HOME>/temp/*
+
+echo "Starting Tomcat..."
+<TOMCAT_HOME>/bin/startup.sh
