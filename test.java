4d5ecb781e857074f92e83a57ecd9529d6fceb74
ramsan21 <ramsan21@gmail.com>
2025-02-20 12:53:17 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,15 +1,5 @@
-helm lint <chart-directory>
-
-helm template <release-name> <chart-directory>
-
-helm template my-release ./my-chart
-
-helm install --dry-run --debug my-release ./my-chart
-
-helm template <chart-directory> | kubectl apply --dry-run=client -f -
-
-helm template <chart-directory> | yamllint -
-
-helm plugin install https://github.com/helm-unittest/helm-unittest
-
-helm unittest <chart-directory>
+xsi:schemaLocation="
+    http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-3.0.xsd
+    http://www.springframework.org/schema/context http://www.springframework.org/schema/context/spring-context-3.0.xsd
+    http://www.springframework.org/schema/tx http://www.springframework.org/schema/tx/spring-tx-3.0.xsd
+    http://cxf.apache.org/jaxws http://cxf.apache.org/schemas/jaxws.xsd">
