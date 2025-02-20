f916930fa6c5606e517a470d1c685b734091521f
ramsan21 <ramsan21@gmail.com>
2025-02-20 18:06:49 +0800

    Update test.java

--- test.java
+++ test.java
@@ -3,3 +3,12 @@ xsi:schemaLocation="
     http://www.springframework.org/schema/context http://www.springframework.org/schema/context/spring-context.xsd
     http://www.springframework.org/schema/tx http://www.springframework.org/schema/tx/spring-tx.xsd
     http://cxf.apache.org/jaxws http://cxf.apache.org/schemas/jaxws.xsd">
+
+<jaxws:endpoint 
+    id="CryptoServiceEndPoint" 
+    implementor="#cryptoService"
+    address="/services/CryptoService"
+    xmlns:s="http://server.uaas.starsec.scb.com"
+    serviceName="s:CryptoServiceService"
+    endpointName="s:CryptoService">
+</jaxws:endpoint>
