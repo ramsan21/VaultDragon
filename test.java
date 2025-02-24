88d54bd4007977d843dde0cba17e731d27cb22d6
ramsan21 <ramsan21@gmail.com>
2025-02-24 18:10:05 +0800

    Update test.java

--- test.java
+++ test.java
@@ -13,43 +13,64 @@ parameters:
     default: aks
 
 stages:
-- stage: Deploy
+# **1st Stage: Deploy to AKS Dev**
+- stage: Deploy_AKS_Dev
+  displayName: "Deploy to AKS Dev"
   jobs:
+  - job: Deploy_AKS_Dev
+    displayName: "Deploying to AKS Dev"
+    pool: sc-linux-devfactory
+    steps:
+    - script: |
+        echo "Deploying to AKS Dev..."
+        helm upgrade --install $(artifactId) ./$(artifactId) \
+          --namespace s2b-security-dev \
+          --values ./$(artifactId)/aks-dev-values.yaml
 
-  # Deploy to AKS (if deployStackName is aks)
-  - ${{ if eq(parameters.deployStackName, 'aks') }}:
-    - job: Deploy_AKS
-      displayName: "Deploy to AKS"
-      pool: sc-linux-devfactory  # Pool for AKS Deployment
+# **2nd Stage: Deploy to AKS QA**
+- stage: Deploy_AKS_QA
+  displayName: "Deploy to AKS QA"
+  dependsOn: Deploy_AKS_Dev  # Runs after AKS Dev
+  jobs:
+  - job: Deploy_AKS_QA
+    displayName: "Deploying to AKS QA"
+    pool: sc-linux-devfactory
+    steps:
+    - script: |
+        echo "Deploying to AKS QA..."
+        helm upgrade --install $(artifactId) ./$(artifactId) \
+          --namespace s2b-security-qa \
+          --values ./$(artifactId)/aks-qa-values.yaml
 
-      steps:
-      - script: echo "Deploying to AKS..."
-      - script: |
-          echo "Setting AKS Kubernetes Parameters"
-          echo "Cluster: 51366-s2bapi-dev-sg-b7cbg"
-          echo "Resource Group: 51366-S2B-API-RG"
-          echo "Namespace: s2b-security-dev"
-      - script: |
-          echo "Deploying using AKS Helm values"
-          helm upgrade --install $(artifactId) ./$(artifactId) \
-            --namespace s2b-security-dev \
-            --values ./$(artifactId)/aks-dev-values.yaml
+# **3rd Stage: Deploy to SKE HK (Runs if Either AKS Dev OR AKS QA is Successful)**
+- stage: Deploy_SKE_HK
+  displayName: "Deploy to SKE HK"
+  dependsOn:
+    - Deploy_AKS_Dev
+    - Deploy_AKS_QA
+  condition: or(succeeded('Deploy_AKS_Dev'), succeeded('Deploy_AKS_QA'))  # Condition to run if any AKS stage succeeds
+  jobs:
+  - job: Deploy_SKE_HK
+    displayName: "Deploying to SKE HK"
+    pool: sc-linux
+    steps:
+    - script: |
+        echo "Deploying to SKE HK..."
+        helm upgrade --install $(artifactId) ./$(artifactId) \
+          --namespace t-26066-s2bsec-s2b-security \
+          --values ./$(artifactId)/stg-prod-values.yaml
 
-  # Deploy to SKE (if deployStackName is skecaasapp)
-  - ${{ if eq(parameters.deployStackName, 'skecaasapp') }}:
-    - job: Deploy_SKE
-      displayName: "Deploy to SKE"
-      pool: sc-linux  # Pool for SKE Deployment
-
-      steps:
-      - script: echo "Deploying to SKE..."
-      - script: |
-          echo "Setting SKE Kubernetes Parameters"
-          echo "Server: https://api.skes006.50933.hk.app.standardchartered.com:6443"
-          echo "Namespace: t-26066-s2bsec-s2b-security"
-      - script: |
-          echo "Deploying using SKE Helm values"
-          helm upgrade --install $(artifactId) ./$(artifactId) \
-            --namespace t-26066-s2bsec-s2b-security \
-            --values ./$(artifactId)/stg-prod-values.yaml \
-            --debug
+# **4th Stage: Deploy to SKE Prod (Only Runs After SKE HK Completes Successfully)**
+- stage: Deploy_SKE_Prod
+  displayName: "Deploy to SKE Prod"
+  dependsOn: Deploy_SKE_HK  # Ensures it runs after SKE HK
+  jobs:
+  - job: Deploy_SKE_Prod
+    displayName: "Deploying to SKE Prod"
+    pool: sc-linux
+    steps:
+    - script: |
+        echo "Deploying to SKE Prod..."
+        helm upgrade --install $(artifactId) ./$(artifactId) \
+          --namespace t-26066-s2bsec-s2b-prod \
+          --values ./$(artifactId)/ske-prod-values.yaml
