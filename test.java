c5b137bf761a42250cd1019d51373b85edcab886
ramsan21 <ramsan21@gmail.com>
2025-02-24 18:21:36 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,9 @@
+trigger:
+  - develop
+  - release/*
+  - feature/*
+  - main
+
 parameters:
   - name: releaseId
     displayName: "Release WorkItem ID"
@@ -11,6 +17,18 @@ parameters:
       - aks
       - skecaasapp
     default: aks
+
+resources:
+  repositories:
+    - repository: governed-templates
+      name: dj-core/governed-templates
+      ref: main
+      type: git
+
+variables:
+  - group: 26066-NonProd
+  - name: artifactId
+    value: "ms-tmx"
 
 stages:
 # **1st Stage: Deploy to AKS Dev**
@@ -27,28 +45,28 @@ stages:
           --namespace s2b-security-dev \
           --values ./$(artifactId)/aks-dev-values.yaml
 
-# **2nd Stage: Deploy to AKS QA**
-- stage: Deploy_AKS_QA
-  displayName: "Deploy to AKS QA"
-  dependsOn: Deploy_AKS_Dev  # Runs after AKS Dev
+# **2nd Stage: Deploy to AKS UAT**
+- stage: Deploy_AKS_UAT
+  displayName: "Deploy to AKS UAT"
+  dependsOn: Deploy_AKS_Dev
   jobs:
-  - job: Deploy_AKS_QA
-    displayName: "Deploying to AKS QA"
+  - job: Deploy_AKS_UAT
+    displayName: "Deploying to AKS UAT"
     pool: sc-linux-devfactory
     steps:
     - script: |
-        echo "Deploying to AKS QA..."
+        echo "Deploying to AKS UAT..."
         helm upgrade --install $(artifactId) ./$(artifactId) \
-          --namespace s2b-security-qa \
-          --values ./$(artifactId)/aks-qa-values.yaml
+          --namespace s2b-security-uat \
+          --values ./$(artifactId)/aks-uat-values.yaml
 
-# **3rd Stage: Deploy to SKE HK (Runs if Either AKS Dev OR AKS QA is Successful)**
+# **3rd Stage: Deploy to SKE HK (Runs if Either AKS Dev or AKS UAT is Successful)**
 - stage: Deploy_SKE_HK
   displayName: "Deploy to SKE HK"
   dependsOn:
     - Deploy_AKS_Dev
-    - Deploy_AKS_QA
-  condition: or(succeeded('Deploy_AKS_Dev'), succeeded('Deploy_AKS_QA'))  # Condition to run if any AKS stage succeeds
+    - Deploy_AKS_UAT
+  condition: or(succeeded('Deploy_AKS_Dev'), succeeded('Deploy_AKS_UAT'))  # Runs if either AKS stage succeeds
   jobs:
   - job: Deploy_SKE_HK
     displayName: "Deploying to SKE HK"
@@ -60,17 +78,17 @@ stages:
           --namespace t-26066-s2bsec-s2b-security \
           --values ./$(artifactId)/stg-prod-values.yaml
 
-# **4th Stage: Deploy to SKE Prod (Only Runs After SKE HK Completes Successfully)**
-- stage: Deploy_SKE_Prod
-  displayName: "Deploy to SKE Prod"
-  dependsOn: Deploy_SKE_HK  # Ensures it runs after SKE HK
+# **4th Stage: Deploy to SKE SG (Only Runs After SKE HK Completes Successfully)**
+- stage: Deploy_SKE_SG
+  displayName: "Deploy to SKE SG"
+  dependsOn: Deploy_SKE_HK  # Runs only after SKE HK
   jobs:
-  - job: Deploy_SKE_Prod
-    displayName: "Deploying to SKE Prod"
+  - job: Deploy_SKE_SG
+    displayName: "Deploying to SKE SG"
     pool: sc-linux
     steps:
     - script: |
-        echo "Deploying to SKE Prod..."
+        echo "Deploying to SKE SG..."
         helm upgrade --install $(artifactId) ./$(artifactId) \
-          --namespace t-26066-s2bsec-s2b-prod \
-          --values ./$(artifactId)/ske-prod-values.yaml
+          --namespace t-26066-s2bsec-s2b-security \
+          --values ./$(artifactId)/stg-prod-values.yaml
