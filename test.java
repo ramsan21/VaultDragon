18f55d1b45f1523aaffedb4a1a9ccd7a668eb875
ramsan21 <ramsan21@gmail.com>
2025-05-09 17:07:01 +0800

    Update test.java

--- test.java
+++ test.java
@@ -5,30 +5,30 @@ import java.time.format.DateTimeFormatter;
 
 public class TimeZoneConverter {
 
-    // Formatter for consistent output
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+    // Custom formatter for input/output
+    private static final DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm:ss");
 
     public static void main(String[] args) {
         // Convert UTC to HKT
-        String utcToHkt = convertUtcToHkt("2025-05-09T10:00:00");
+        String utcToHkt = convertUtcToHkt("09-05-25 10:00:00");
         System.out.println("UTC to HKT: " + utcToHkt);
 
         // Convert HKT to UTC
-        String hktToUtc = convertHktToUtc("2025-05-09T18:00:00");
+        String hktToUtc = convertHktToUtc("09-05-25 18:00:00");
         System.out.println("HKT to UTC: " + hktToUtc);
     }
 
     public static String convertUtcToHkt(String utcDateTimeStr) {
-        LocalDateTime utcDateTime = LocalDateTime.parse(utcDateTimeStr);
+        LocalDateTime utcDateTime = LocalDateTime.parse(utcDateTimeStr, customFormatter);
         ZonedDateTime utcZoned = utcDateTime.atZone(ZoneId.of("UTC"));
         ZonedDateTime hktZoned = utcZoned.withZoneSameInstant(ZoneId.of("Asia/Hong_Kong"));
-        return hktZoned.format(formatter);
+        return hktZoned.format(DateTimeFormatter.ofPattern("dd-MM-yy HH:mm:ss z"));
     }
 
     public static String convertHktToUtc(String hktDateTimeStr) {
-        LocalDateTime hktDateTime = LocalDateTime.parse(hktDateTimeStr);
+        LocalDateTime hktDateTime = LocalDateTime.parse(hktDateTimeStr, customFormatter);
         ZonedDateTime hktZoned = hktDateTime.atZone(ZoneId.of("Asia/Hong_Kong"));
         ZonedDateTime utcZoned = hktZoned.withZoneSameInstant(ZoneId.of("UTC"));
-        return utcZoned.format(formatter);
+        return utcZoned.format(DateTimeFormatter.ofPattern("dd-MM-yy HH:mm:ss z"));
     }
 }
