cd8d8330808e386c3bdc667060e46e4f739f1573
ramsan21 <ramsan21@gmail.com>
2025-05-22 20:50:50 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,10 +1,29 @@
-AuthorizationCodeGrant codeGrant = new AuthorizationCodeGrant(new AuthorizationCode("dummyCode"), new URI("https://dummy.redirect.uri"));
-when(request.getParameterMap()).thenReturn(new HashMap<>());
-...
-when(tokenRequest.getAuthorizationGrant()).thenReturn(codeGrant);
+@Test
+void testTokens_CodeGrant_Success() throws Exception {
+    // Do not mock AuthorizationGrant
+    AuthorizationCode code = new AuthorizationCode("dummyAuthCode");
+    URI redirectUri = new URI("https://dummy.redirect.uri");
+    AuthorizationCodeGrant codeGrant = new AuthorizationCodeGrant(code, redirectUri);
 
+    TokenRequest tokenRequest = new TokenRequest(
+        new URI("https://dummy.token.endpoint"),
+        new ClientID("client-id"),
+        codeGrant
+    );
 
-AuthorizationCodeGrant codeGrant = new AuthorizationCodeGrant(new AuthorizationCode("auth-code"), new URI("https://dummy.uri"));
-when(tokenRequest.getAuthorizationGrant()).thenReturn(codeGrant);
+    // If your controller pulls this from the request, mock accordingly
+    when(request.getParameterMap()).thenReturn(new HashMap<>());
 
-AuthorizationCodeGrant codeGrant = (AuthorizationCodeGrant) grant;
+    // Inject your constructed TokenRequest (with the real grant)
+    when(requestValidator.validate(any())).thenReturn(
+        ValidationResult.builder()
+            .success(true)
+            .tokenRequest(tokenRequest)
+            .relyingParty("dummyRP")
+            .user(new User("dummyUser"))
+            .tokens(new Tokens("access-token", "refresh-token"))
+            .build()
+    );
+
+    // Remaining mocking for response/writer/assertions...
+}
