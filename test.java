5ae29598e237ca339ed837a5ddff5efbe629ccf5
ramsan21 <ramsan21@gmail.com>
2025-06-05 11:06:30 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,7 +1,53 @@
-SELECT 
-  TimeStamp,
-  TO_CHAR(
-    TO_DATE('1970-01-01','YYYY-MM-DD') + (TimeStamp / 1000) / 86400,
-    'YYYY-MM-DD HH24:MI:SS'
-  ) AS readable_time
-FROM your_table;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+import com.hazelcast.core.EntryEvent;
+import com.hazelcast.map.MapEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CacheEntryListenerTest {
+
+    private CacheEntryListener listener;
+    private EntryEvent<String, Object> mockEvent;
+    private MapEvent mockMapEvent;
+
+    @BeforeEach
+    public void setup() {
+        listener = new CacheEntryListener();
+        mockEvent = mock(EntryEvent.class);
+        mockMapEvent = mock(MapEvent.class);
+
+        when(mockEvent.getKey()).thenReturn("sampleKey");
+        when(mockEvent.getValue()).thenReturn("sampleValue");
+        when(mockMapEvent.getName()).thenReturn("sampleMap");
+    }
+
+    @Test
+    public void testEntryAdded() {
+        listener.entryAdded(mockEvent);
+    }
+
+    @Test
+    public void testEntryRemoved() {
+        listener.entryRemoved(mockEvent);
+    }
+
+    @Test
+    public void testEntryEvicted() {
+        listener.entryEvicted(mockEvent);
+    }
+
+    @Test
+    public void testEntryExpired() {
+        listener.entryExpired(mockEvent);
+    }
+
+    @Test
+    public void testMapEvicted() {
+        listener.mapEvicted(mockMapEvent);
+    }
+}
