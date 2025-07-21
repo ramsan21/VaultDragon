7c23a842bd5f30bf09576caeb0c262a393aa3bf8
ramsan21 <ramsan21@gmail.com>
2025-07-21 14:15:47 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,12 +1,16 @@
-- name: Remove run.sh and rename catalyst_run.sh to run.sh if ENV is htg
-  when: ENV == 'htg'
+- name: Remove run.sh and rename ctrun.sh to run.sh if ENV is STG or SIT3
+  when: ENV in ['STG', 'SIT3']
   block:
     - name: Remove existing run.sh
       file:
         path: "{{ temp_dir }}/run.sh"
         state: absent
 
-    - name: Rename catalyst_run.sh to run.sh
-      command: mv catalyst_run.sh run.sh
+    - name: Rename ctrun.sh to run.sh if it exists
+      command: mv ctrun.sh run.sh
       args:
         chdir: "{{ temp_dir }}"
+      when: 
+        - lookup('ansible.builtin.file', temp_dir + '/ctrun.sh') is not none
+        - lookup('ansible.builtin.file', temp_dir + '/ctrun.sh') != ''
+        - ansible.builtin.stat(path=temp_dir + '/ctrun.sh').stat.exists
