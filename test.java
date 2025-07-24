82f735660cdf64477c81c3ad80a26d068d3ffa4e
ramsan21 <ramsan21@gmail.com>
2025-07-24 20:57:49 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,11 @@
-dest: >-
-  {{ temp_dir }}/{{ item | regex_replace('^' + STAGING_DIR + '/', '') | regex_replace(ENV if ENV in ['STG', 'SIT3'] else '', '') }}
-
-
-
 - name: Copy the files with secrets to server
   copy:
     src: "{{ item }}"
     dest: >-
-      {{ temp_dir }}/{{ item | regex_replace('^' + STAGING_DIR + '/', '') | regex_replace(ENV if ENV in ['STG', 'SIT3'] else '', '') }}
+      {{ temp_dir }}/{{ item
+        | regex_replace('^' + STAGING_DIR + '/', '')
+        | regex_replace('/' + ENV + '/', '/' if ENV in ['STG', 'SIT3'] else '/' + ENV + '/')
+      }}
     owner: starsswb
     group: starsed
     mode: 0755
