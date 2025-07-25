c201712ead225e9a806a1c4ecada5c8e154c4ead
ramsan21 <ramsan21@gmail.com>
2025-07-25 16:46:55 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,1 @@
-git checkout catalyst/main
-git pull origin catalyst/main
-git checkout -b hard-reset-main
-
-
-git reset --hard b458c3e5
-
-git add -A
-git commit -m "Restoring catalyst/main to content of commit b458c3e5"
-
-git push origin hard-reset-main
+dest: "{{ temp_dir }}/{{ item | regex_replace('^' + STAGING_DIR + '/', '') | regex_replace('/' + ENV + '/', '/') }}"
