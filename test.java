4afb90fb6a44f1cb1534aac1e94e1487a95bb895
ramsan21 <ramsan21@gmail.com>
2025-07-29 22:02:09 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,6 @@
-kubectl logs <pod-name> -n <namespace> > pod-logs.txt
+kubectl describe pod <pod-name> -n <namespace>
+
+
+kubectl get pod <pod-name> -n <namespace> -o jsonpath="{.status.containerStatuses[*].lastState.terminated.reason}"
+
+kubectl logs <pod-name> -n <namespace> --previous
