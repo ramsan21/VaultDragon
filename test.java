75853e3fc10a5ea383ff9574702053255a0a5532
ramsan21 <ramsan21@gmail.com>
2025-07-29 22:08:09 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,2 @@
-kubectl describe pod <pod-name> -n <namespace>
-
-
-kubectl get pod <pod-name> -n <namespace> -o jsonpath="{.status.containerStatuses[*].lastState.terminated.reason}"
-
-kubectl logs <pod-name> -n <namespace> --previous
+kubectl get pod ms-tmx-7948669875-qgfcq -n t-26066-s2bsec-s2b-security \
+  -o jsonpath="{.status.containerStatuses[0].lastState.terminated.reason}"
